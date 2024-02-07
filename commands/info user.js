8e605d10aa16af3b35082b29c016bd15e9e27079
ToddyTheNoobDud <86982643+ToddyTheNoobDud@users.noreply.github.com>
2024-02-07 17:07:21 -0300

    Update info user.js
    Remastered (Rework) info user

--- commands/info user.js
+++ commands/info user.js
@@ -1,8 +1,8 @@
 // This code is still beign made, Bugs  and crashes will occur!
 // Todo: Make 2 versions (1 for users and 1 for bot), Optimize the code, Make an error handler
-// Done: Version for users, Versions for bots, Optimized Code (Extra: fixed some crashes)
+// Done: Version for users, Versions for bots, Optimized Code, Crash handler [Beta] (Extra: fixed some crashes)
 // This code will only work in servers, Universal version will work on servers, dms, etc
-import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
+import { ApplicationCommandOptionType, EmbedBuilder, blockQuote } from "discord.js";
 
 export const Command = {
   name: "info",
@@ -37,125 +37,111 @@ export const Command = {
   ],
   //@ts-ignore
   run: async (client, interaction) => {
-    const subcommand = interaction.options.getSubcommand();
-    if (!interaction.inGuild()) {
-      return await interaction.reply({
-        content: "This command can only be used in guilds",
-        ephemeral: true,
-      });
-    }
-    if (subcommand === "server_user") {
-      const user = interaction.options.getUser("user") || interaction.user;
-      if (user.bot) {
+    try {
+      const subcommand = interaction.options.getSubcommand();
+      if (!interaction.inGuild()) {
         return await interaction.reply({
-          content: "Please select a user, not a bot",
+          content: "This command can only be used in guilds",
           ephemeral: true,
         });
       }
-
-      const embed = new EmbedBuilder()
-        .setColor("NotQuiteBlack")
-        .setAuthor({
-            name: user.username,
-            iconURL: user.avatarURL({ dynamic: true, extension: 'png' })
-        })
-        .setFooter({
-            text: 'made by mushroom0162',
-            iconURL: client.user.avatarURL({ dynamic: true, extension: 'png' })
-        })
-        .addFields(
-          { name: "Username", value: user.username, inline: true },
-          {
-            name: "Created At",
-            value: user.createdAt.toDateString(),
-            inline: true,
-          },
-          {
-            name: "Flags",
-            value: user.flags.toArray().join(", ") || "None",
-            inline: true,
-          },
-          { name: "Id", value: user.id, inline: true },
-          {
-            name: "Is from System",
-            value: user.system ? "Yes" : "No",
-            inline: true,
-          },
-          {
-            name: "Is verified",
-            value: user.verified ? "Yes" : "No",
-            inline: true,
-          },
-          {
-            name: "Is a partial",
-            value: user.partial ? "Yes" : "No",
-            inline: true,
-          },
-          {
-            name: "Avatar URL",
-            value: `[Avatar URL](${user.avatarURL({
-              dynamic: true,
-              extension: "png",
-            })})`,
-            inline: true,
-          },
-          {
-            name: "Banner URL",
-            value: user.bannerURL({ dynamic: true, extension: "png" || "gif" })
-              ? `[Banner](${user.bannerURL({
-                  dynamic: true,
-                  extension: "png" || "gif",
-                })})`
-              : "No URL",
-            inline: true,
-          }
-        );
-
-      await interaction.reply({ embeds: [embed] });
-    } else if (subcommand === "server_bot") {
-      const bot = interaction.options.getUser("bot");
-      if (!bot.bot) {
-        return await interaction.reply({
-          content: "Please select a bot, not a user",
-          ephemeral: true,
-        });
-      }
-
-      const embed = new EmbedBuilder()
-        .setColor("NotQuiteBlack")
-        .setAuthor({
-            name:  bot.username,
-            iconURL:  bot.avatarURL({ dynamic: true, extension: "png" })
+      if (subcommand === "server_user") {
+        const user = interaction.options.getUser("user") || interaction.user;
+        if (user.bot === true) {
+          return await interaction.reply({
+            content: "Please select a user, not a bot",
+            ephemeral: true,
+          });
         }
-        )
-        .setFooter({
-            text: "made by mushroom0162",
-           iconURL: client.user.avatarURL({ dynamic: true, extension: "png" })
-        })
-        .addFields(
-          { name: "Username", value: bot.username, inline: true },
-          { name: "ID", value: bot.id, inline: true },
-          { name: "Is System", value: bot.system ? "Yes" : "No", inline: true },
-          {
-            name: "Created At",
-            value: bot.createdAt.toDateString(),
-            inline: true,
-          },
-          {
-            name: "Avatar URL",
-            value: `[Avatar URL](${bot.avatarURL({
-              dynamic: true,
-              extension: "png",
-            })})`,
-            inline: true,
-          },
-          {
-            name: "is a Partial",
-            value: bot.partial ? "yes" : "no",
-            inline: true,
+        const flags = {
+          ActiveDeveloper: "👨‍💻・Active Developer",
+          BugHunterLevel1: "🐛・Discord Bug Hunter",
+          BugHunterLevel2: "🐛・Discord Bug Hunter",
+          CertifiedModerator: "👮‍♂️・Certified Moderator",
+          HypeSquadOnlineHouse1: "🏠・House Bravery Member",
+          HypeSquadOnlineHouse2: "🏠・House Brilliance Member",
+          HypeSquadOnlineHouse3: "🏠・House Balance Member",
+          HypeSquadEvents: "🏠・HypeSquad Events",
+          PremiumEarlySupporter: "👑・Early Supporter",
+          Partner: "👑・Partner",
+          Staff: "👨‍💼・Discord Staff",
+          TeamPseudoUser: "👨‍💼・Discord Team",
+          VerifiedBot: "🤖・Verified Bot",
+          VerifiedDeveloper: "👨‍💻・(early)Verified Bot Developer",
+        }
+        const userFlags = user.flags.toArray() || [];
+        const createdatt = (user.createdTimestamp / 1000).toFixed(0);
+        const joinedat = (user.joinedTimestamp / 1000).toFixed(0); //this code don't work, don't try using it, it will return NaN
+  
+        const embed = new EmbedBuilder()
+          .setColor("Aqua")
+          .setDescription(`
+          **• Username: ** ${`\``}${user.username}${`\``}
+                **• ID: ** ${`\``}${user.id}${`\``}
+          **• Discriminator: ** ${`\``}${user.discriminator}${`\``}
+          **• Avatar URL: ** [URL](${user.avatarURL({ dynamic: true, extension: 'png' })})
+          **• Bot: ** ${user.bot ? "Yes" : "No"}
+          **• Created At: ** <t:${createdatt}>
+          **• Joined At: ** Still in development
+          **• Flags: ** ${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}
+          `)
+          .setAuthor({
+              name: (`info about ${user.username}`),
+              iconURL: user.avatarURL({ dynamic: true, extension: 'png' })
+          })
+          .setFooter({
+              text: 'made by mushroom0162',
+              iconURL: client.user.avatarURL({ dynamic: true, extension: 'png' })
+          })
+  
+        await interaction.reply({ embeds: [embed] });
+      } else if (subcommand === "server_bot") {
+          const bot = interaction.options.getUser("bot");
+          if (!bot.bot) {
+            return await interaction.reply({
+              content: "Please select a bot, not a user",
+              ephemeral: true,
+            });
           }
-        );
-      await interaction.reply({ embeds: [embed] });
+    
+          const embed = new EmbedBuilder()
+            .setColor("NotQuiteBlack")
+            .setAuthor({
+                name:  bot.username,
+                iconURL:  bot.avatarURL({ dynamic: true, extension: "png" })
+            }
+            )
+            .setFooter({
+                text: "made by mushroom0162",
+               iconURL: client.user.avatarURL({ dynamic: true, extension: "png" })
+            })
+            .addFields(
+              { name: "Username", value: bot.username, inline: true },
+              { name: "ID", value: bot.id, inline: true },
+              { name: "Is System", value: bot.system ? "Yes" : "No", inline: true },
+              {
+                name: "Created At",
+                value: bot.createdAt.toDateString(),
+                inline: true,
+              },
+              {
+                name: "Avatar URL",
+                value: `[Avatar URL](${bot.avatarURL({
+                  dynamic: true,
+                  extension: "png",
+                })})`,
+                inline: true,
+              },
+              {
+                name: "is a Partial",
+                value: bot.partial ? "yes" : "no",
+                inline: true,
+              }
+            );
+          await interaction.reply({ embeds: [embed] });
+        }
+      } catch(error) {
+        console.log(error)
+      }
     }
-  },
-};
+  }
